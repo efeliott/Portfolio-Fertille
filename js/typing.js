4524d5b643da38afe96800e6c9615fad60f95a49
Fertille <efeliott@gmail.com>
2023-01-18 07:36:25 +0100

    step in progress

--- js/typing.js
+++ js/typing.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:["","Développeur Web", "Etudiant en BTS SIO"],
+    strings:["","Développeur Web", "Etudiant en BTS SIO", "Développeur App Mobile"],
     typeSpeed:100,
     BackSpeed:80,
     loop:true
