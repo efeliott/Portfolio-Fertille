4ba729fb8a7fbdacb457d19d506a16d97b0ca611
Eliott <efeliott@gmail.com>
2023-02-09 15:53:21 +0100

    stage te projets en cours

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+// Code pour ouvrir et fermer le pop-up
+document.getElementById("open-popup").onclick = function() {
+    document.getElementById("popup").style.display = "block";
+  };
+  
+  document.getElementById("close-popup").onclick = function() {
+    document.getElementById("popup").style.display = "none";
+  };
+  
