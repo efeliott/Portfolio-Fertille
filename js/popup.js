6a62e4f7e9ec6685ca02e27056f2ec525b7bcd44
Eliott <efeliott@gmail.com>
2023-03-16 12:00:00 +0100

    a finir

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,18 @@
 // Code pour ouvrir et fermer le pop-up
 document.getElementById("open-popup").onclick = function() {
     document.getElementById("popup").style.display = "block";
-  };
+};
   
   document.getElementById("close-popup").onclick = function() {
     document.getElementById("popup").style.display = "none";
-  };
+};
+
+// Code pour ouvrir et fermer le pop-up 2
+document.getElementById("open-popup-2").onclick = function() {
+  document.getElementById("popup-2").style.display = "block";
+};
+  
+document.getElementById("close-popup-2").onclick = function() {
+  document.getElementById("popup-2").style.display = "none";
+};
   
