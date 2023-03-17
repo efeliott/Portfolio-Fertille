88245a3623deaa5d2dd8aff57299d59f24ddf76b
Fertille <efeliott@gmail.com>
2023-03-17 22:11:04 +0100

    veille a faire / verifier responsive

--- js/timeline.js
+++ js/timeline.js
@@ -1,8 +1,26 @@
+const school = $('.timeline-container-school');
+const work = $('.timeline-container-work');
+const btn_school = $('.btn-school');
+const btn_work = $('.btn-work');
+
 $('.btn-school').on('click' , function(){
-    $('.timeline-container-school').addClass('active');
-    $('.timeline-container-work').removeClass('active');
+    if($('.timeline-container-school').hasClass("active")) {
+    }
+    else{
+        school.addClass('active');
+        work.removeClass('active');
+        btn_school.addClass('active');
+        btn_work.removeClass('active');
+    }
 });
 
-$('.close-btn').on('click' , function(){
-    $('.sidebar').removeClass('active');
+$('.btn-work').on('click' , function(){
+    if($('.timeline-container-work').hasClass("active")) { 
+    }
+    else{
+        work.addClass('active');
+        school.removeClass('active');
+        btn_school.removeClass('active');
+        btn_work.addClass('active');
+    }
 });
