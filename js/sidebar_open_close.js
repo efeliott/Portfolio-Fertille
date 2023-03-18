f52e98a94678e681161e34c7d246b07efe1af49c
Fertille <efeliott@gmail.com>
2023-03-18 23:47:32 +0100

    veille a faire / verifier responsive

--- js/sidebar_open_close.js
+++ js/sidebar_open_close.js
@@ -5,3 +5,7 @@ $('.open-btn').on('click' , function(){
 $('.close-btn').on('click' , function(){
     $('.sidebar').removeClass('active');
 });
+
+$('.nav-link').on('click' , function(){
+    $('.sidebar').removeClass('active');
+});
